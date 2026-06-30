196e0617b64087b848f61bd85b8b8a0a96da5251
iamlenasamba-cyber <iamlenasamba@gmail.com>
2026-06-30 22:15:22 +0000

    question1

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
 // array
+
+$categories=[];
+$categories=[
+    0=>["nom"=>"alimentaire",
+        "code"=>"1020",
+        "produits"=>[
+            0=> [
+                "nom"=> "lait",
+                "reference"=>"a2kv1",
+                "qte"=>20,
+                "prix"=>100
+            ],
+             1=> [
+                "nom"=> "riz",
+                "reference"=>"3hfT6",
+                "qte"=>10,
+                "prix"=>600
+            ],
+        ]
+    ],
+     1=>["nom"=>"hygiene",
+        "code"=>"4568",
+        "produits"=>[],
+    ]
+];
